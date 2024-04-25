Bobbar/PolyPlane

commit 62bf917e414cbfa64e14555fa47f6baa9ff9b31a

- Move vapor trails forward slightly - Slow vapor trails more quickly. - More efficient cloud rendering. Remove excess allocations.

PolyPlane/Rendering/Cloud.cs
@@ -1,24 +1,23 @@
 ﻿using PolyPlane.GameObjects;
-using unvell.D2DLib;
 
 namespace PolyPlane.Rendering
 {
     public class Cloud
     {
         public D2DPoint Position;
-        public List<D2DPoint> Points = new List<D2DPoint>();
+        public readonly D2DPoint[] PointsOrigin = Array.Empty<D2DPoint>();
+        public D2DPoint[] Points = Array.Empty<D2DPoint>();
         public List<D2DPoint> Dims = new List<D2DPoint>();
         public float Rotation = 0f;
-        public D2DColor Color;
         public float Radius = 0f;
 
-        public Cloud(D2DPoint position, List<D2DPoint> points, List<D2DPoint> dims, float rotation, D2DColor color)
+        public Cloud(D2DPoint position, List<D2DPoint> points, List<D2DPoint> dims, float rotation)
         {
             Position = position;
-            Points = points;
+            PointsOrigin = points.ToArray();
+            Points = points.ToArray();
             Dims = dims;
             Rotation = rotation;
-            Color = color;
         }
 
         public static Cloud RandomCloud(Random rnd, D2DPoint position, int minPoints, int maxPoints, int minRadius, int maxRadius)
@@ -31,20 +30,20 @@ namespace PolyPlane.Rendering
             var radius = rnd.Next(minRadius, maxRadius);
             var dims = new List<D2DPoint>();
 
-
             // Try to make clouds at higher altitude more thin and whispy?
             var altFact = Helpers.Factor(Math.Abs(position.Y), MAX_ALT);
 
             for (int i = 0; i < nPnts; i++)
-                dims.Add(new D2DPoint(rnd.NextFloat(MIN_DIMS + (altFact * ALT_FACT_AMT), MAX_DIMS + (altFact * ALT_FACT_AMT)), rnd.NextFloat(MIN_DIMS - (altFact * ALT_FACT_AMT), MAX_DIMS - (altFact * ALT_FACT_AMT))));
+            {
+                var dimsX = rnd.NextFloat(MIN_DIMS + (altFact * ALT_FACT_AMT), MAX_DIMS + (altFact * ALT_FACT_AMT));
+                var dimsY = rnd.NextFloat(MIN_DIMS + (altFact * ALT_FACT_AMT), MAX_DIMS + (altFact * ALT_FACT_AMT));
+                dims.Add(new D2DPoint(dimsX, dimsY));
+            }
 
             var rotation = rnd.NextFloat(0, 360);
             var pnts = GameObjectPoly.RandomPoly(nPnts, radius).ToList();
-            var color1 = new D2DColor(0.2f, D2DColor.White);
-            var color2 = new D2DColor(0.2f, D2DColor.Gray);
-            var color = Helpers.LerpColor(color1, color2, rnd.NextFloat(0f, 1f));
 
-            var newCloud = new Cloud(position, pnts, dims, rotation, color);
+            var newCloud = new Cloud(position, pnts, dims, rotation);
             newCloud.Radius = radius;
 
             return newCloud;

PolyPlane/GameObjects/FighterPlane.cs
@@ -634,7 +634,7 @@ namespace PolyPlane.GameObjects
             if (isControl && _controlWing == null)
                 _controlWing = wing;
 
-            _vaporTrails.Add(new Vapor(wing, D2DPoint.Zero, 2f));
+            _vaporTrails.Add(new Vapor(wing, new D2DPoint(5f, 0f), 2f));
 
             Wings.Add(wing);
         }

PolyPlane/GameObjects/Vapor.cs
@@ -121,7 +121,7 @@ namespace PolyPlane.GameObjects
             {
                 base.Update(dt, viewport, renderScale);
 
-                this.Velocity += -this.Velocity * 0.9999f * dt;
+                this.Velocity += -this.Velocity * (dt * 1.5f);
 
                 _ellipse.origin = this.Position;
 

PolyPlane/Rendering/RenderManager.cs
@@ -254,10 +254,8 @@ namespace PolyPlane.Rendering
 
         public void ResizeGfx(bool force = false)
         {
-            var curSize = GetViewportScaled();
-
             if (!force)
-                if (World.ViewPortBaseSize.width == curSize.Width && World.ViewPortBaseSize.height == curSize.Height)
+                if (World.ViewPortBaseSize.width == this.Width && World.ViewPortBaseSize.height == this.Height)
                     return;
 
             _device?.Resize();
@@ -1081,8 +1079,8 @@ namespace PolyPlane.Rendering
             var color1 = new D2DColor(1f, DARKER_COLOR, DARKER_COLOR, DARKER_COLOR);
             var color2 = D2DColor.WhiteSmoke;
 
-            var points = cloud.Points.ToArray();
-            Helpers.ApplyTranslation(points, points, cloud.Rotation, cloud.Position, SCALE);
+            var points = cloud.Points;
+            Helpers.ApplyTranslation(cloud.PointsOrigin, cloud.Points, cloud.Rotation, cloud.Position, SCALE);
 
             // Find min/max height.
             var minY = points.Min(p => p.Y);
@@ -1110,7 +1108,6 @@ namespace PolyPlane.Rendering
                 // Add time of day color.
                 color = AddToDColor(color);
 
-                //ctx.FillEllipse(new D2DEllipse(point, new D2DSize(dims.X, dims.Y)), cloud.Color);
                 ctx.FillEllipse(new D2DEllipse(point, new D2DSize(dims.X, dims.Y)), color);
             }
         }
@@ -1128,7 +1125,7 @@ namespace PolyPlane.Rendering
                 float rotDir = 1f;
 
                 // Fiddle rotation direction.
-                if (cloud.Points.Count % 2 == 0)
+                if (cloud.Points.Length % 2 == 0)
                     rotDir = -1f;
 
                 cloud.Rotation = Helpers.ClampAngle(cloud.Rotation + (3f * rotDir) * dt);

commit db7a2c1985d6a17955b5c7a258fc0c66b7c62329

Fix cloud dimentions broken in 62bf917e414cbfa64e14555fa47f6baa9ff9b31a

PolyPlane/Rendering/Cloud.cs
@@ -36,7 +36,7 @@ namespace PolyPlane.Rendering
             for (int i = 0; i < nPnts; i++)
             {
                 var dimsX = rnd.NextFloat(MIN_DIMS + (altFact * ALT_FACT_AMT), MAX_DIMS + (altFact * ALT_FACT_AMT));
-                var dimsY = rnd.NextFloat(MIN_DIMS + (altFact * ALT_FACT_AMT), MAX_DIMS + (altFact * ALT_FACT_AMT));
+                var dimsY = rnd.NextFloat(MIN_DIMS - (altFact * ALT_FACT_AMT), MAX_DIMS - (altFact * ALT_FACT_AMT));
                 dims.Add(new D2DPoint(dimsX, dimsY));
             }
 