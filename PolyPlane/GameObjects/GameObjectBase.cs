Bobbar/PolyPlane

commit c3d388e6c8dc648f22ff5c6e62820325059d1bde

Remove previous hacky attachment fix and just update all attachments inside Update.

PolyPlane/GameObjects/GameObjectBase.cs
@@ -330,13 +330,7 @@ namespace PolyPlane.GameObjects
             DoUpdate(dt);
 
             UpdateTimers(dt);
-            UpdateAttachments(dt);
-
-            // Hack: Net objects need this because their position updates are buffered
-            // and only interpolated within DoUpdate. We need to make sure attachments
-            // like wings are synced after the interpolated position is applied.
-            if (this.IsNetObject)
-                UpdatePhysicsAttachments(0f);
+            UpdateAllAttachments(dt);
         }
 
         private void AdvancePositionAndRotation(float dt)

commit d367b119e864bf94f7808d570c788512d496b267

Fix for incorrect flight physics broken since c3d388e6c8dc648f22ff5c6e62820325059d1bde

PolyPlane/GameObjects/GameObjectBase.cs
@@ -330,7 +330,7 @@ namespace PolyPlane.GameObjects
             DoUpdate(dt);
 
             UpdateTimers(dt);
-            UpdateAllAttachments(dt);
+            UpdateAttachments(dt);
         }
 
         private void AdvancePositionAndRotation(float dt)
@@ -367,6 +367,9 @@ namespace PolyPlane.GameObjects
             {
                 var now = World.CurrentNetTimeTicks();
                 InterpBuffer.InterpolateState(now);
+
+                // Update physics attachments (like wings) after interpolating a new state.
+                UpdatePhysicsAttachments(0f);
             }
             else
             {