b91e11e639b47d76de70d8f6e47cfe84659c4a68
Bobbar <bobbar@gmail.com>
2025-05-08 15:38:51 -0400

    Missiles: Fix damping and more handling tweaks.

--- PolyPlane/GameObjects/GuidedMissile.cs
+++ PolyPlane/GameObjects/GuidedMissile.cs
@@ -53,6 +53,7 @@ namespace PolyPlane.GameObjects
         private const float THRUST = 2500f;
         private const float FUEL = 10f;
         private const float DEFLECTION_RATE = 45f;
+        private const float DEFLECTION_DAMPING = 1.2f;
 
         private float _currentFuel = 0f;
         private float _gForce = 0f;
@@ -199,21 +200,21 @@ namespace PolyPlane.GameObjects
         private void InitWings()
         {
             const float liftScale = 1.5f;
-            const float minVelo = 600f;
+            const float minVelo = 700f;
 
             _tailWing = new Wing(this, new WingParameters()
             {
                 RenderLength = 2.5f,
                 RenderWidth = 1f,
                 Area = 0.1f,
-                MaxDeflection = 50f,
-                MaxLiftForce = 3000f * liftScale,
-                VeloLiftFactor = 2f,
+                MaxDeflection = 45f,
+                MaxLiftForce = 4500f * liftScale,
+                VeloLiftFactor = 1f,
                 VeloDragFactor = 1f,
                 PivotPoint = new D2DPoint(-21f, 0f),
                 Position = new D2DPoint(-22f, 0f),
                 MinVelo = minVelo,
-                ParasiticDrag = 0.9f,
+                ParasiticDrag = 0.7f,
                 DragFactor = 0.8f,
                 DeflectionRate = DEFLECTION_RATE,
                 MaxAOA = 40f
@@ -223,13 +224,13 @@ namespace PolyPlane.GameObjects
             {
                 RenderLength = 0f,
                 Area = 0.045f,
-                MaxLiftForce = 2000f * liftScale,
-                VeloLiftFactor = 2f,
+                MaxLiftForce = 1000f * liftScale,
+                VeloLiftFactor = 1f,
                 VeloDragFactor = 1f,
                 MinVelo = minVelo,
-                ParasiticDrag = 0.5f,
+                ParasiticDrag = 0.2f,
                 MaxAOA = 30f,
-                DragFactor = 0.4f,
+                DragFactor = 0.5f,
             });
 
             _noseWing = new Wing(this, new WingParameters()
@@ -239,12 +240,12 @@ namespace PolyPlane.GameObjects
                 Area = 0.06f,
                 MaxDeflection = 20f,
                 MaxLiftForce = 2000f * liftScale,
-                VeloLiftFactor = 2f,
+                VeloLiftFactor = 1f,
                 VeloDragFactor = 1f,
                 Position = new D2DPoint(21.5f, 0f),
                 MinVelo = minVelo,
-                ParasiticDrag = 0.5f,
-                DragFactor = 0.4f,
+                ParasiticDrag = 0.2f,
+                DragFactor = 0.5f,
                 MaxAOA = 30f
             });
 
@@ -306,11 +307,9 @@ namespace PolyPlane.GameObjects
                     nextDeflect = Utilities.Lerp(nextDeflect, nextDeflect * (aoaFact * rotSpdFact * spdFact), fuelFact);
                 }
 
-                // Damp the deflection.
-                nextDeflect = Utilities.Damp(_tailWing.Deflection, nextDeflect, 80f, dt);
-
-                _tailWing.Deflection = TAIL_AUTH * -nextDeflect;
-                _noseWing.Deflection = NOSE_AUTH * nextDeflect;
+                // Damp and apply deflection.
+                _tailWing.Deflection = TAIL_AUTH * Utilities.Damp(_tailWing.Deflection, -nextDeflect, DEFLECTION_DAMPING, dt);
+                _noseWing.Deflection = NOSE_AUTH * Utilities.Damp(_noseWing.Deflection, nextDeflect, DEFLECTION_DAMPING, dt);
 
                 this.Deflection = _tailWing.Deflection;
 
